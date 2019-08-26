1768972429078092126582c16dc7b730f0d7945a
chenfu <chenfu@yuwo.com>
2019-08-26 13:39:02 +0800

    提交代码

---
+++ git_idea/src/com/huang/GitIdea.java
@@ -0,0 +1,12 @@
+package com.huang;
+
+public class GitIdea {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("good");
+        System.out.println("huang");
+        System.out.println("gao");
+        System.out.println("che");
+        System.out.println("yu");
+    }
+}
